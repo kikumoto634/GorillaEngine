41362e89838156ca17afa4a6f28ceec039248a24
634_3 <k020g1146@g.neec.ac.jp>
2022-11-30 15:51:05 +0900

    DirectXCommon,Debug時の抑制エラー

--- Engine/base/DirectXCommon.cpp
+++ Engine/base/DirectXCommon.cpp
@@ -159,6 +159,31 @@ HRESULT DirectXCommon::CreateDevice()
 			break;
 		}
 	}
+
+#ifdef _DEBUG
+	ComPtr<ID3D12InfoQueue> infoQueue;
+	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue)))) {
+		// 抑制するエラー
+		D3D12_MESSAGE_ID denyIds[] = {
+		  /*
+		   * Windows11でのDXGIデバッグレイヤーとDX12デバッグレイヤーの相互作用バグによるエラーメッセージ
+		   * https://stackoverflow.com/questions/69805245/directx-12-application-is-crashing-in-windows-11
+		   */
+		  D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE};
+		// 抑制する表示レベル
+		D3D12_MESSAGE_SEVERITY severities[] = {D3D12_MESSAGE_SEVERITY_INFO};
+		D3D12_INFO_QUEUE_FILTER filter{};
+		filter.DenyList.NumIDs = _countof(denyIds);
+		filter.DenyList.pIDList = denyIds;
+		filter.DenyList.NumSeverities = _countof(severities);
+		filter.DenyList.pSeverityList = severities;
+		// 指定したエラーの表示を抑制する
+		infoQueue->PushStorageFilter(&filter);
+		// エラー時にブレークを発生させる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+	}
+#endif
+
 	return result;
 }
 
