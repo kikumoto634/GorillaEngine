ea73ce604b9741f4a79434b5fa813111b35bd06a
k020g1146 <k020g1146@g.neec.ac.jp>
2022-12-06 14:52:02 +0900

    平行ライトの実装

--- Resources/shader/ObjPS.hlsl
+++ Resources/shader/ObjPS.hlsl
@@ -13,18 +13,23 @@ float4 main(VSOutput input) : SV_TARGET
 	const float shiness = 4.0f;
 	////頂点から視点への方向ベクトル
 	float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
-	////ライトに向かうベクトルと法線の内積
-	//float3 dotlightnormal = dot(lightv, input.normal);
-	////反射光ベクトル
-	//float3 reflect = normalize(-lightv + 2 * dotlightnormal * input.normal);
-	////環境反射光
-	//float3 ambient = m_ambient;
-	////拡散反射光
-	//float3 diffuse = dotlightnormal * m_diffuse;
-	////鏡面反射光
-	//float3 specular = pow(saturate(dot(reflect, eyedir)), shiness) * m_specular;
-	////すべて加算
-	//shadecolor.rgb = (ambient + diffuse + specular) * lightcolor;
+	
+	// 平行光源
+	for (int i = 0; i < DIRECTIONALLIGHT_NUM; i++) {
+		if (dirLights[i].active) {
+			// ライトに向かうベクトルと法線の内積
+			float3 dotlightnormal = dot(dirLights[i].lightv, input.normal);
+			// 反射光ベクトル
+			float3 reflect = normalize(-dirLights[i].lightv + 2 * dotlightnormal * input.normal);
+			// 拡散反射光
+			float3 diffuse = dotlightnormal * m_diffuse;
+			// 鏡面反射光
+			float3 specular = pow(saturate(dot(reflect, eyedir)), shiness) * m_specular;
+
+			// 全て加算する
+			shadecolor.rgb += (diffuse + specular) * dirLights[i].lightcolor;
+		}
+	}
 
 	// 点光源
 	for (int i = 0; i < POINTLIGHT_NUM; i++) {
